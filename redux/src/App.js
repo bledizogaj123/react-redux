4e4ee1b61a9ec7e717c9dbdacac8a087e367968d
bledizogaj123 <bz47140@ubt-uni.net>
2023-02-24 09:58:40 +0100

    Initial Create

---
+++ redux/src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({type: 'INC'});
+  }
+  const decrement = () => {
+
+  }
+  return (
+    <div >
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}> Increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+}
+
+export default App;
